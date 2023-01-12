b6abf48262715f4a69a8733e75075686951c9624
dsxkline <934476300@qq.com>
2023-01-12 14:41:43 +0800

    update setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_namespace_packages
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+setup(
+    name="pydsxkline",  # 包名称
+    version="1.1.1",  # 版本号
+    author="fangyunsm",  # 作者
+    author_email="934476300@qq.com",  # 作者邮箱
+    description="pydsxkline是一个有趣的python包，一行代码即可显示K线图，主要应用于股票金融量化领域，当您想要把股票数据图形化的时候，可以试试这个小工具，希望能帮到有需要的朋友。",  # 描述
+    long_description=readme(),  # 长文描述
+    keywords="",  # 项目关键词
+    url="https://github.com/dsxkline/dsxkline_python",  # 项目主页
+    license="MIT License",  # 许可证
+    packages=find_namespace_packages('pydsxkline'),
+    install_requires=['pywebview'],
+    python_requires='>=3.6,<4'
+)
